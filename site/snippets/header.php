00c8635828fa89ffba19536272bd14ca9d6225df
Jason Hegyessy <hegyessy@MacBook.local>
2016-02-13 18:34:09 -0800

    Link style files

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>L'atmosphere home decorating</title>
+	<?php echo css('assets/css/reset.css') ?>
+	<?php echo css('assets/css/site.css') ?>
 </head>
 <body>
 	<header>
