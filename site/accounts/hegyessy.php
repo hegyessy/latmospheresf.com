f3a3ee1a7dde828f6863ef811c14e1b7a6b34b80
Jason Hegyessy <hegyessy@MacBook.local>
2016-02-13 17:57:55 -0800

    Kirby blank site template

---
+++ site/accounts/hegyessy.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: hegyessy
+email: jason@hegyessy.com
+password: >
+  $2a$10$E0LfuLr/TM7wdjIDLD2hyuPff9FMEbAMqldew89MDRP2L1K/ndU4q
+language: en
+role: admin
+history:
+  - home
