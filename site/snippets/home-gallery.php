69ec7681e8b6e16bf40ebd3930f7be400efcd1a0
Jason Hegyessy <hegyessy@MacBook.local>
2016-02-20 17:04:10 -0800

    fetch gallery thumbs from gallery page, not homepage.

--- site/snippets/home-gallery.php
+++ site/snippets/home-gallery.php
@@ -1,8 +1,8 @@
 <div id="gallery">
 	<h2>Gallery</h2>
 	<div id="gallery-images">
-		<?php foreach($page->images() as $image): ?>
-			<?php echo thumb($image, array('width' => 256, 'crop' => true)); ?>
-	 	<?php endforeach ?>
+	 	<?php foreach($pages->gallery->images()->limit(10) as $image): ?>
+	 		<?php echo thumb($image, array('width' => 200, 'crop' => true)); ?>
+	 	<?php endforeach ?>
 	 </div>
 </div>
