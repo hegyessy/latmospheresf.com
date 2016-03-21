a91f7c72352731717936e574a7a4253945eacb20
hegyessy <jason@hegyessy.com>
2016-03-21 10:50:07 -0700

    Remove redundant content or files.

--- site/templates/default.php
+++
@@ -1,9 +0,0 @@
-<div id="gallery">
-
-	<?php foreach($pages->gallery->images()->limit(10) as $key => $image): ?>
-		<div id="<?php echo $key ?>" class="slide" style="background-image:url(<?php echo $image->url() ?>);"></div>
-	<?php endforeach ?>
-	<div class="button_next">Next</div>
-	<div class="button_prev">Prev</div>
-	<div class="button_close">Close</div>
-</div>
