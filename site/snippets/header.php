15f88a2bcb1ca0929c407abf2b720478ebbec057
hegyessy <jason@hegyessy.com>
2016-03-27 17:20:23 -0700

    Start making the site responsive

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>L'atmosphere home decorating</title>
 	<?php echo css('assets/css/reset.css') ?>
 	<?php echo css('assets/css/site.css') ?>
