0ce0ed82e26bd726b54d41721ac648582e40a3a0
hegyessy <jason@hegyessy.com>
2016-04-17 15:25:22 -0700

    hack to only allow numbers spaces and dashes

--- site/snippets/form.php
+++ site/snippets/form.php
@@ -6,7 +6,7 @@
 
 	<input type="text" class="form-element contact-input" id="name" name="name" placeholder="Name (required)" required>
 	<input type="email" class="form-element contact-input" id="email" name="email" placeholder="Email (required)" required>
-	<input type="tel" class="form-element contact-input" id="phone" name="phone" placeholder="Phone number (optional)">
+	<input type="tel" onkeypress="return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 32 || event.charCode 45" class="form-element contact-input" id="phone" name="phone" placeholder="Phone number (optional)">
 	<input type="submit" class="form-element contact-button" name="submit">
 
 </form>
