22c97a25ce4140f2183316940ec92b045ed487ce
Jason Hegyessy <hegyessy@Odens-oga.local>
2016-06-10 15:47:01 -0700

    Account changes

---
+++ site/accounts/hegyessy-dev.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: hegyessy-dev
+email: jason@hegyessy.com
+password: >
+  $2a$10$wWWTCtZdTB7L5UYREm1HxOcB.OhwNeIU.DqaVsd/6reMSuaSFkJEu
+language: en
+role: admin
+history:
+  - about
---
+++ site/accounts/ingridternynck.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: ingridternynck
+firstname: Ingrid
+lastname: Ternynck
+email: ingrid.ternynck@gmail.com
+password: >
+  $2a$10$ZPmKVaz7eRQgPcuLGhsBx.LIoQWkgAQKhsMSWPP3YYkHsD7NeEWg6
+language: en
+role: admin
