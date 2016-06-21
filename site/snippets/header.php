b05f3561a8d424b7045851d358a2358be1bd974b
Jason Hegyessy <jason@hegyessy.com>
2016-06-20 22:16:02 -0700

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>L'Atmosphere home decorating</title>
+	<title>L'Atmosphere - Ingrid Ternynck - Interior Design</title>
 	<?php echo css('assets/css/reset.css') ?>
 	<?php echo css('assets/css/site.css') ?>
 </head>
