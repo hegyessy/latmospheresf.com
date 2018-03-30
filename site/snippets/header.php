c9d9db407a6c341f36077a63e9b4e38547b78134
Jason Hegyessy <jason@hegyessy.com>
2018-03-30 11:37:10 -0700

    Site update test
    Check to see if site can be updated through Github integration

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Mar 30, 2018 -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>L'Atmosphere - Ingrid Ternynck - Interior Design</title>
